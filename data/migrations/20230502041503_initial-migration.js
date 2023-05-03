e87fae8a66217d3316e7a08b6f98528e951dc839
Krygore <webdev.krygore@gmail.com>
2023-05-02 19:50:53 -0700

    wip

--- data/migrations/20230502041503_initial-migration.js
+++ data/migrations/20230502041503_initial-migration.js
@@ -2,14 +2,30 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = async function(knex) {
+  await knex.schema
+    .createTable('recipes', table => {
+        table.increments()
+    })
+    .createTable('ingredients', table => {
+        table.increments()
+    })
+    .createTable('steps', table => {
+        table.increments()
+    })
+    .createTable('step_ingredients', table => {
+        table.increments()
+    })
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+  await knex.schema
+    .dropTableIfExists('step_ingredients')
+    .dropTableIfExists('steps')
+    .dropTableIfExists('ingredients')
+    .dropTableIfExists('recipes')
 };
